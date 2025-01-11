2a08bc98e8a904ec4ca05ac7e63d9d0e0db790ae
SalimKaneki <salimpereira01@gmail.com>
2025-01-11 13:11:36 +0000

    Fix bug de la date de naissance

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -1,11 +1,4 @@
 <x-guest-layout>
-    <div class="container position-sticky z-index-sticky top-0">
-        <div class="row">
-            <div class="col-12">
-                <x-guest.sidenav-guest />
-            </div>
-        </div>
-    </div>
     <main class="main-content mt-0">
         <section>
             <div class="page-header min-vh-100">
@@ -16,11 +9,9 @@
                                 <div class="card-header pb-0 text-left bg-transparent text-center">
                                     <h3 class="font-weight-black text-dark display-6">Connexion</h3>
                                     <p class="mb-0">Bienvenue sur Gest-School</p>
-                                    <p class="mb-0">Pour accéder à votre espace, veuillez vous connecter<br></p>
-                                    <!-- <p class="mb-0">Identifiants de démonstration :</p>
-                                    <p class="mb-0">Email : <b>admin@corporateui.com</b></p>
-                                    <p class="mb-0">Mot de passe : <b>secret</b></p> -->
+                                    <p class="mb-0">Pour accéder à votre espace, veuillez vous connecter</p>
                                 </div>
+                                
                                 <div class="text-center">
                                     @if (session('status'))
                                         <div class="mb-4 font-medium text-sm text-green-600">
@@ -33,6 +24,7 @@
                                         </div>
                                     @enderror
                                 </div>
+
                                 <div class="card-body">
                                     <form role="form" class="text-start" method="POST" action="sign-in">
                                         @csrf
@@ -42,12 +34,14 @@
                                                 placeholder="Entrez votre adresse email"
                                                 aria-label="Email" aria-describedby="email-addon">
                                         </div>
+                                        
                                         <label>Mot de passe</label>
                                         <div class="mb-3">
                                             <input type="password" id="password" name="password"
                                                 class="form-control" placeholder="Entrez votre mot de passe"
                                                 aria-label="Password" aria-describedby="password-addon">
                                         </div>
+                                        
                                         <div class="d-flex align-items-center">
                                             <div class="form-check form-check-info text-left mb-0">
                                                 <input class="form-check-input" type="checkbox" value=""
@@ -59,18 +53,13 @@
                                             <a href="{{ route('password.request') }}"
                                                 class="text-xs font-weight-bold ms-auto">Mot de passe oublié ?</a>
                                         </div>
+                                        
                                         <div class="text-center">
-                                            <button type="submit" class="btn btn-dark w-100 mt-4 mb-3">Connexion</button>
-                                            <button type="button" class="btn btn-white btn-icon w-100 mb-3">
-                                                <span class="btn-inner--icon me-1">
-                                                    <img class="w-5" src="../assets/img/logos/google-logo.svg"
-                                                        alt="logo-google" />
-                                                </span>
-                                                <span class="btn-inner--text">Connexion avec Google</span>
-                                            </button>
+                                            <button type="submit" class="btn btn-dark w-100 mt-4 mb-3">Se connecter</button>
                                         </div>
                                     </form>
                                 </div>
+
                                 <div class="card-footer text-center pt-0 px-lg-2 px-1">
                                     <p class="mb-4 text-xs mx-auto">
                                         Nouvel utilisateur ?
@@ -79,12 +68,12 @@
                                 </div>
                             </div>
                         </div>
+
                         <div class="col-md-6">
                             <div class="position-absolute w-40 top-0 end-0 h-100 d-md-block d-none">
                                 <div class="oblique-image position-absolute fixed-top ms-auto h-100 z-index-0 bg-cover ms-n8"
                                     style="background-image:url('../assets/img/image-sign-in.jpg')">
-                                    <div
-                                        class="blur mt-12 p-4 text-center border border-white border-radius-md position-absolute fixed-bottom m-4">
+                                    <div class="blur mt-12 p-4 text-center border border-white border-radius-md position-absolute fixed-bottom m-4">
                                         <h2 class="mt-3 text-dark font-weight-bold">Votre Portail Éducatif</h2>
                                         <h6 class="text-dark text-sm mt-5">Copyright © 2024 - Tous droits réservés</h6>
                                     </div>
@@ -97,3 +86,4 @@
         </section>
     </main>
 </x-guest-layout>
+

0870b7454b65da95c6b133035162a8ba519f6348
SalimKaneki <salimpereira01@gmail.com>
2025-01-22 09:42:54 +0000

    user ID addition

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,13 +17,19 @@ class DatabaseSeeder extends Seeder
         //     'password' => Hash::make('secret'),
         //     'about' => "Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality).",
         // ]);
+        
+        // User::factory()->create([
+        //     'name' => '@dmin',
+        //     'email' => 'admin@gest-school.com',
+        //     'password' => Hash::make('@dminsecret'),
+        //     'about' => "Apllication de gestion d'école.",
+        // ]);
+
         User::factory()->create([
-            'name' => '@dmin',
-            'email' => 'admin@gest-school.com',
-            'password' => Hash::make('@dminsecret'),
-            'about' => "Apllication de gestion d'école.",
+            'name' => 'Ecole Test',
+            'email' => 'testeur@gest-platform.com',
+            'password' => Hash::make('Ecole01@2025'), 
+            'about' => 'Utilisateur test pour expérimenter la plateforme de gestion d’école.',
         ]);
-
-
     }
 }

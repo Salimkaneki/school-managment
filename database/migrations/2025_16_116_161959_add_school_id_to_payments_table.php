8824102d2ad2869b26a705af29bd864c75792bce
Salimkaneki <salimpereira01@gmail.com>
2025-02-27 08:42:52 +0000

    Rename of files

--- database/migrations/2025_16_116_161959_add_school_id_to_payments_table.php
+++ database/migrations/2025_16_116_161959_add_school_id_to_payments_table.php
@@ -1,33 +1,33 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('payments', function (Blueprint $table) {
-            $table->foreignId('school_id')
-                  ->after('student_id')
-                  ->constrained('schools')
-                  ->onDelete('cascade');
-        });
-    }
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      */
+//     public function up(): void
+//     {
+//         Schema::table('payments', function (Blueprint $table) {
+//             $table->foreignId('school_id')
+//                   ->after('student_id')
+//                   ->constrained('schools')
+//                   ->onDelete('cascade');
+//         });
+//     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('payments', function (Blueprint $table) {
-            //
-            $table->dropForeign(['school_id']);
-            $table->dropColumn('school_id');
-        });
-    }
-};
+//     /**
+//      * Reverse the migrations.
+//      */
+//     public function down(): void
+//     {
+//         Schema::table('payments', function (Blueprint $table) {
+//             //
+//             $table->dropForeign(['school_id']);
+//             $table->dropColumn('school_id');
+//         });
+//     }
+// };
